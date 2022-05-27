9589212efc24d3a8c9c18c8da5933440c7d3ddfe
iniffur <jamesruffini97@gmail.com>
2022-05-27 18:28:59 +0100

    multiple changes / gitignore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,43 +3,42 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-import { createNotEmittedStatement, idText } from "typescript";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-    const [items, setItems] = useState([]);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-    function addItem(addNewNote) {
-        setItems(prevItems => {
-            return [...prevItems, addNewNote];
-        });
-    }
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
-    function deleteNote(id) {
-        setItems(prevItems => {
-            return prevItems.filter((noteItem, index) => {
-                return index !== id;
-            });
-        });
-    }
-
-
-    return (
-        <div>
-            <Header />
-            <CreateArea onAdd={addItem} />
-            {items.map((noteItem, index) => {
-                return <Note 
-                title={noteItem.title} 
-                content={noteItem.content} 
-                onDelete={deleteNote} 
-                key={index}
-                id={index} />;
-            })}
-
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
